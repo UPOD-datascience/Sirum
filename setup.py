eeca1712d8bac8d8e97c6f3fd04ffd1e33d8f8a6
Huibert - Jan Joosse <hjjoosse@Huiberts-MacBook-Pro.local>
2020-03-30 12:44:42 +0200

    code_rep development

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Sirum',
+    version='0.0.1',
+    packages=['src', 'src.data', 'src.models'],
+    url='',
+    license='',
+    author='Bram van Es, Danial Senejohnny, Malin Overmars, Michael Niemantsverdriet, Leon Reteig, Huibert-Jan Joosse',
+    author_email='',
+    description='Development of Corona SIR-like model'
+)
