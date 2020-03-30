cb9b4e95883404bcf3d041ccfc83cc66ac9caf3c
Huibert - Jan Joosse <hjjoosse@Huiberts-MacBook-Pro.local>
2020-03-30 14:07:48 +0200

    code_rep

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     license='',
     author='Bram van Es, Danial Senejohnny, Malin Overmars, Michael Niemantsverdriet, Leon Reteig, Huibert-Jan Joosse',
     author_email='',
-    description='Development of Corona SIR-like model'
+    description='Development of SIR-like model'
 )
