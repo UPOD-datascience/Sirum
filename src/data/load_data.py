602aa8b2577b9dc06d83988303a1784e96391619
Huibert - Jan Joosse <hjjoosse@Huiberts-MacBook-Pro.local>
2020-04-01 16:55:26 +0200

    data loading

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -9,23 +9,26 @@ class CovidDataSelection:
         self.url_cases = cfg.URL_CASES
         self.url_deaths = cfg.URL_DEATHS
         self.url_recovered = cfg.URL_RECOVERED
-        self.data = pd.DataFrame()
 
     def create_data(self):
-        self.data.append([self._load_cases(), self._load_deaths(), self._load_recovered()],
+        self.data = self._load_cases()
+        self.data = self.data.append([self._load_deaths(), self._load_recovered()],
                          ignore_index=True)
 
     def _load_cases(self):
         cases = pd.read_csv(self.url_cases)
-        cases = cases.loc[self.data['Country/Region'] == self.country]
+        cases = cases.loc[cases['Country/Region'] == self.country]
+        cases['type'] = 'cases'
         return cases
 
     def _load_deaths(self):
         deaths = pd.read_csv(self.url_deaths)
-        deaths = deaths.loc[self.data['Country/Region'] == self.country]
+        deaths = deaths.loc[deaths['Country/Region'] == self.country]
+        deaths['type'] = 'deaths'
         return deaths
 
     def _load_recovered(self):
         recovered = pd.read_csv(self.url_recovered)
-        recovered = recovered.loc[self.data['Country/Region'] == self.country]
+        recovered = recovered.loc[recovered['Country/Region'] == self.country]
+        recovered['type'] = 'recovered'
         return recovered
