6a2fe72c983e5ee84df277e5f57c6bf9186ae9f1
Danial <d.senejohnny@gmail.com>
2020-04-14 10:54:05 +0200

    Revert "renaming"
    This reverts commit 43577c2cc31079b4ab44e7325628166424533794.

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(
     packages=setuptools.find_packages(),
     url='',
     license='',
-    install_requires=['pandas', 'numpy'],
     author='Bram van Es, Danial Senejohnny, Malin Overmars, Michael Niemantsverdriet, Leon Reteig, Huibert-Jan Joosse',
     author_email='',
     description='Development of SIR-like model'
